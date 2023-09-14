5dedb5c350108dfc4c65541e7e66c479f6c1d9b7
Douglas-Augusto <douglas.full.ti@gmail.com>
2023-09-14 19:38:30 -0300

    14/09/2023-19:38

---
+++ src/main/java/list/OperacoesBasicas/ListaTarefas.java
@@ -0,0 +1,9 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.List;
+
+public class ListaTarefas {
+    //atributo
+    private List<Tarefa> tarefaList;
+    
+}
--- src/main/java/list/OperacoesBasicas/Tarefa.java
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -1,19 +1,14 @@
 package main.java.list.OperacoesBasicas;
 
 public class Tarefa {
-  //atributo
-  private String descricao;
+    //Atributo
+    private String descricao;
 
-  public Tarefa(String descricao) {
-    this.descricao = descricao;
-  }
+    public String getDescricao() {
+        return descricao;
+    }
 
-  public String getDescricao() {
-    return descricao;
-  }
-
-  @Override
-  public String toString() {
-    return  descricao;
-  }
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
 }
